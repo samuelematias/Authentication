0a309275d9cded81e23162da9c4218e91f58c42c
Samuel Mataraso <samuelmataraso@MacBook-Air-de-Samuel-Mataraso.local>
2018-12-31 12:13:59 -0300

    addding firebase lib

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+	componentDidMount() {
+		firebase.initializeApp({
+			apiKey: 'AIzaSyD0U0ttY3u9oRFydBgFXJ_6ZlpLyYX0IRo',
+			authDomain: 'authentication-b97b6.firebaseapp.com',
+			databaseURL: 'https://authentication-b97b6.firebaseio.com',
+			projectId: 'authentication-b97b6',
+			storageBucket: 'authentication-b97b6.appspot.com',
+			messagingSenderId: '220782362811'
+		});
+	}
+
 	render() {
 		return (
 			<View>
