66d76d2aa5614057efe68a9cf3b9856b39ef3668
Samuel Mataraso <samuelmataraso@MacBook-Air-de-Samuel-Mataraso.local>
2018-12-31 12:38:32 -0300

    create Input component.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { text: '' };
+	}
 	render() {
+		const { text } = this.state;
 		return (
-			<View>
-				<Card>
-					<CardSection />
-					<CardSection />
-					<CardSection>
-						<Button>{'Log in'}</Button>
-					</CardSection>
-				</Card>
-			</View>
+			<Card>
+				<CardSection>
+					<TextInput
+						style={{
+							height: 20,
+							flex: 1,
+							width: null
+						}}
+						onChangeText={text => this.setState({ text })}
+						value={text}
+					/>
+				</CardSection>
+				<CardSection />
+				<CardSection>
+					<Button>{'Log in'}</Button>
+				</CardSection>
+			</Card>
 		);
 	}
 }
